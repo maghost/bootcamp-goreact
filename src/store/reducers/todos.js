4ab83114d5e6131f308f1207ab5f04df819ba501
Matheus Marques <matheus_maghost@hotmail.com>
2019-05-19 16:17:23 -0300

    Add prototype and create TodoList

---
+++ src/store/reducers/todos.js
@@ -0,0 +1,8 @@
+const INITIAL_STATE = [{ id: 1, text: 'Fazer café' }, { id: 2, text: 'Estudar ReactJS' }];
+
+export default function todos(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
