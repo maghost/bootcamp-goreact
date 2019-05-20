cfedc14398c982ad2af31d8a25b977418c93bce2
Matheus Marques <matheus_maghost@hotmail.com>
2019-05-20 10:18:45 -0300

    Validate if already favorited

--- src/store/sagas/favorites.js
+++ src/store/sagas/favorites.js
@@ -1,11 +1,20 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import api from '../../services/api';
 
 import { Creators as FavoriteActions } from '../ducks/favorites';
 
 export function* addFavorite(action) {
   try {
-    const { data } = yield call(api.get, `/repos/${action.payload.repository}`);
+    const favoriteName = action.payload.repository;
+    const isDuplicated = yield select(state =>
+      state.favorites.data.find(favorite => favorite.name === favoriteName));
+
+    if (isDuplicated) {
+      yield put(FavoriteActions.addFavoriteFailure('Repositório duplicado'));
+      return;
+    }
+
+    const { data } = yield call(api.get, `/repos/${favoriteName}`);
 
     const repositoryData = {
       id: data.id,
