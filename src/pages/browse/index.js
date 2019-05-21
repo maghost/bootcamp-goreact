39d2cd09b394607a4052ca862a9010a3449bb705
Matheus Marques <matheus_maghost@hotmail.com>
2019-05-21 12:39:34 -0300

    Add routes and create browse page

---
+++ src/pages/browse/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { Container, Title, List, Playlist } from './styles';
+
+const Browse = () => (
+  <Container>
+    <Title>Navegar</Title>
+
+    <List>
+      <Playlist href="">
+        <img
+          src="https://i.scdn.co/image/3a75fdf63322a8fdc86951eb836542093872530b"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>Relaxe enquanto você programa ouvindo apenas as melhores do rock mundial!</p>
+      </Playlist>
+      <Playlist href="">
+        <img
+          src="https://i.scdn.co/image/3a75fdf63322a8fdc86951eb836542093872530b"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>Relaxe enquanto você programa ouvindo apenas as melhores do rock mundial!</p>
+      </Playlist>
+      <Playlist href="">
+        <img
+          src="https://i.scdn.co/image/3a75fdf63322a8fdc86951eb836542093872530b"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>Relaxe enquanto você programa ouvindo apenas as melhores do rock mundial!</p>
+      </Playlist>
+      <Playlist href="">
+        <img
+          src="https://i.scdn.co/image/3a75fdf63322a8fdc86951eb836542093872530b"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>Relaxe enquanto você programa ouvindo apenas as melhores do rock mundial!</p>
+      </Playlist>
+    </List>
+  </Container>
+);
+
+export default Browse;
