7a48cd5b8c25c34cad07c353b2376cda82a80269
Matheus Marques <matheus_maghost@hotmail.com>
2019-05-21 11:14:54 -0300

    Change App to add a Sidebar and Player

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import GlobalStyle from './styles/global';
 
+import Sidebar from './components/Sidebar';
+import Player from './components/Player';
+
+import { Wrapper, Container } from './styles/components';
+
 const App = () => (
-  <Fragment>
+  <Wrapper>
     <GlobalStyle />
-    <h1>Hello World</h1>
-  </Fragment>
+    <Container>
+      <Sidebar />
+    </Container>
+    <Player />
+  </Wrapper>
 );
 
 export default App;
